9527e30f5f6d54be2a77524411f114441c968a92
Fabio Classo <fabioclasso@floripacodegurus.com.br>
2019-08-25 21:55:25 -0300

    modelvideo

---
+++ modelvideo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fabio/Desktop/repositories/django_projects/modelvideo/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
